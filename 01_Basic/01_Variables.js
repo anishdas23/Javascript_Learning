15d34bb96b37d3551e534b35fa91b8ae919c91a5
Anish Das <121731710+anishdas23@users.noreply.github.com>
2023-12-18 13:56:35 +0000

    DataTypes Done

--- 01_Basic/01_Variables.js
+++ 01_Basic/01_Variables.js
@@ -1,6 +1,8 @@
 const accountId= 152018878
 let accountEmail="anishdas@gmail.com"
-var accountPassword="12345" // Prefer not to use var in code because of issue in block and funcational scope
+var accountPassword="12345"
+ /* Prefer not to use var in code
+because of issue in block and funcational scope */    
 accountCity="Kolkata"
 
 let accountState;
---
+++ 01_Basic/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict" // treate all js code as never version
+// alert("HIII") we are using node.js, not browers 
+// ECMA tc39 // MDN 
+
+//DATA TYPES
+// number
+// string ="",''
+// boolean => true/false
+// bigint
+// null => standalone value
+// undefined => 
+// symbol => unique
+// object
+
+let roll = 25
+let name = "Anish Das"
+let collageName = "Techno India Hooghly"
+console.log(typeof roll );
+console.log(typeof null );
+console.log(typeof undefined );
