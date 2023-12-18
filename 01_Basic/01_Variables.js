c8e40af61e257fb6ac973950b598c56d77a17b5c
Anish Das <121731710+anishdas23@users.noreply.github.com>
2023-12-18 13:36:52 +0000

    Add Variables Done

---
+++ 01_Basic/01_Variables.js
@@ -0,0 +1,11 @@
+const accountId= 152018878
+let accountEmail="anishdas@gmail.com"
+var accountPassword="12345" // Prefer not to use var in code because of issue in block and funcational scope
+accountCity="Kolkata"
+
+let accountState;
+// accountId= 1458789  TypeError: Assignment to constant variable. NOT ALLOWED
+
+
+console.log(accountId) // Simple print funcation
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
